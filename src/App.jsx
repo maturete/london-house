646e5945c2a3efe623d922f997ed6c270e0bcfe7
maturete <69656874+maturete@users.noreply.github.com>
2020-12-08 18:25:14 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css';
+import './components/general/NavBar/NavBar.css';
+import NavBar from './components/general/NavBar/NavBar.jsx';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+    </>
+  );
+}
+
+export default App;
